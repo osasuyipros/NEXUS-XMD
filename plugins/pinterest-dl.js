d744c6c503f2c8409a13e37b1869f4f9c2f25bdb
Pkdriller <technexus613@gmail.com>
2025-07-09 17:00:52 +0300

    Update pinterest-dl.js

--- plugins/pinterest-dl.js
+++ plugins/pinterest-dl.js
@@ -32,7 +32,7 @@ cmd({
         const videoUrl = media.find(item => item.type.includes('720p'))?.download_url || media[0].download_url;
 
         // Prepare the new message with the updated caption
-        const desc = `╭━━━〔 *KHAN-MD* 〕━━━┈⊷
+        const desc = `╭━━━〔 *NEXUS-XMD* 〕━━━┈⊷
 ┃▸╭───────────
 ┃▸┃๏ *PINS DOWNLOADER*
 ┃▸└───────────···๏
@@ -41,7 +41,7 @@ cmd({
 ┇๏ *Title* - ${title}
 ┇๏ *Media Type* - ${media[0].type}
 ╰━━❑━⪼
-> *© Pᴏᴡᴇʀᴇᴅ Bʏ KʜᴀɴX-Aɪ ♡*`;
+> *© Pᴏᴡᴇʀᴇᴅ Bʏ pkdriller ♡*`;
 
         // Send the media (video or image) to the user
         if (videoUrl) {
